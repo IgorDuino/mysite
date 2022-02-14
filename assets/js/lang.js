4b794d9321d6430cd66578a95b2ec91b9fe8271a
Igor <tkdomigor@gmail.com>
2022-02-14 19:58:15 +0300

    working to follow figma

--- assets/js/lang.js
+++ assets/js/lang.js
@@ -1,40 +1,42 @@
 const lang_btn = document.querySelector("#lang_switcher_btn");
 
 function switch_lang() {
-    if (localStorage.getItem("lang") === "ru") {
+    if (localStorage.getItem("lang") === "rus") {
         localStorage.setItem("lang", "eng")
         lang_btn.innerHTML = 'eng'
-    } else{
-        localStorage.setItem("lang", "ru")
-        lang_btn.innerHTML = 'ru'
+    } else {
+        localStorage.setItem("lang", "rus")
+        lang_btn.innerHTML = 'rus'
     }
-    change_lang(lang_btn.innerHTML)
+    load_lang(lang_btn.innerHTML)
 
 }
 
 lang_btn.addEventListener('click', switch_lang)
 
-function change_lang(lang_code) {
-    const requestURLEng = `languages/${lang_code}.json`;
-
-    const request = new XMLHttpRequest();
-    request.open('GET', requestURLEng);
-    request.send();
-
-    request.onload = function () {
-        const res = JSON.parse(request.responseText);
-        lang = res
-    }
-
+function change_lang(lang) {
+    console.log(lang)
     for (let key in lang) {
         try {
             console.log(lang[key])
             document.querySelector('.' + key).innerHTML = lang[key]
-        } catch (e){
+        } catch (e) {
             console.error(e)
         }
     }
 }
 
+function load_lang(code) {
+    const requestURLEng = `/languages/${code}.json`;
 
-change_lang(lang_btn.innerHTML)
+    const request = new XMLHttpRequest();
+    request.open('GET', requestURLEng);
+    request.send();
+    let r;
+    request.onload = function () {
+        const res = JSON.parse(request.responseText)
+        change_lang(res)
+    }
+}
+
+load_lang(lang_btn.innerHTML)
